4b333d1a0ac147c2d58e14b4a99aef68c166ab8c
shroouq <37282287+shroouq@users.noreply.github.com>
2023-04-14 23:29:11 -0700

    Create producttype.php

---
+++ php/classes/producttype.php
@@ -0,0 +1,69 @@
+<?php
+include_once("contr_data.php");
+include_once("setData.php");
+
+
+interface producttype{
+    public function addproduc();
+ }
+
+
+ class Book  implements producttype {
+      public function __construct($sku, $name, $price ,$type,  $weight){
+         $this -> sku = $sku;
+         $this -> price = $price;
+         $this -> name = $name;
+         $this -> type = $type;
+         $this -> weight = $weight;
+      }
+    public function addproduc(){
+        
+        $data= array('sku'=>$this->sku,'name'=>$this->name,'price'=>$this->price , 'type'=>$this->type , 'weight'=>$this->weight);
+        $addproduct = new Contr();
+        $addproduct->addpro($this->sku, $data);
+    }
+ }
+ class Furniture  implements producttype {
+    public function __construct($sku, $name, $price ,$type, $size){
+       $this -> sku = $sku;
+       $this -> price = $price;
+       $this -> name = $name;
+       $this -> type = $type;
+       $this -> size = $size;
+    }
+  public function addproduc(){
+      
+      $data= array('sku'=>$this->sku,'name'=>$this->name,'price'=>$this->price , 'type'=>$this->type , 'size'=>$this->size);
+      $addproduct = new Contr();
+      $addproduct->addpro($this->sku, $data);
+  }
+}
+class Furnitureh  implements producttype {
+    public function __construct($sku, $name, $price ,$type, $dimensions){
+       $this -> sku = $sku;
+       $this -> price = $price;
+       $this -> name = $name;
+       $this -> type = $type;
+       $this -> length = $dimensions;
+
+      
+    }
+  public function addproduc(){
+
+      $data= array('sku'=>$this->sku,'name'=>$this->name,'price'=>$this->price , 'type'=>$this->type , 'dimensions'=>$this->dimensions);
+      $addproduct = new Contr();
+      $addproduct->addpro($this->sku, $data);
+  }
+}
+
+
+ class Datadd{
+
+ function attribute( $sku, $name, $price ,$type, $size, $weight, $dimensions) {
+
+    $class = $type;
+    $d =  new $class( $sku, $name, $price ,$type, $size, $weight, $dimensions);
+    $d->addproduc();
+}
+ }
+ 
