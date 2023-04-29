1a3e6854ae8433bdf39aaa22bf18df68b2714749
shroouq <37282287+shroouq@users.noreply.github.com>
2023-04-28 19:27:36 -0700

    Update Footr.js

--- src/component/Footr.js
+++ src/component/Footr.js
@@ -4,11 +4,11 @@ import "../headr.css";
 
 
 
-function Footr(){
-    return(
-    <div className="footer">
-        <h3> scandiweb test assignment </h3>
-    </div>
+function Footr() {
+    return (
+        <div className="footer">
+            <h3> scandiweb test assignment </h3>
+        </div>
     )
 }
 
