697bf90fd5cf4d2c76f777d8c0d432aad4aabf62
shroouq <37282287+shroouq@users.noreply.github.com>
2023-04-28 19:20:16 -0700

    Update producttype.php

--- php/classes/producttype.php
+++ php/classes/producttype.php
@@ -1,54 +1,65 @@
 <?php
+
 include_once("contr_data.php");
 include_once("setData.php");
 
 
-interface producttype{
+interface producttype
+{
     public function addproduc();
- }
+}
 
 
- class Book  implements producttype {
-      public function __construct($sku, $name, $price ,$type,  $size,  $weight, $dimensions){
-         $this -> sku = $sku;
-         $this -> price = $price;
-         $this -> name = $name;
-         $this -> type = $type;
-         $this -> weight = $weight;
-      }
-    public function addproduc(){
-        
+class Book implements producttype
+{
+    public function __construct($sku, $name, $price, $type, $size, $weight)
+    {
+        $this -> sku = $sku;
+        $this -> price = $price;
+        $this -> name = $name;
+        $this -> type = $type;
+        $this -> weight = $weight;
+    }
+    public function addproduc()
+    {
+
         $data= array('sku'=>$this->sku,'name'=>$this->name,'price'=>$this->price , 'type'=>$this->type , 'weight'=>$this->weight);
         $addproduct = new Contr();
         $addproduct->addpro($this->sku, $data);
     }
- }
- class Furniture  implements producttype {
-    public function __construct($sku, $name, $price ,$type, $size){
-       $this -> sku = $sku;
-       $this -> price = $price;
-       $this -> name = $name;
-       $this -> type = $type;
-       $this -> size = $size;
+}
+class DVD implements producttype
+{
+    public function __construct($sku, $name, $price, $type, $size)
+    {
+        $this -> sku = $sku;
+        $this -> price = $price;
+        $this -> name = $name;
+        $this -> type = $type;
+        $this -> size = $size;
     }
-  public function addproduc(){
-      
+  public function addproduc()
+  {
+
       $data= array('sku'=>$this->sku,'name'=>$this->name,'price'=>$this->price , 'type'=>$this->type , 'size'=>$this->size);
       $addproduct = new Contr();
       $addproduct->addpro($this->sku, $data);
   }
 }
-class Furnitureh  implements producttype {
-    public function __construct($sku, $name, $price ,$type, $size,  $weight, $dimensions){
-       $this -> sku = $sku;
-       $this -> price = $price;
-       $this -> name = $name;
-       $this -> type = $type;
-       $this -> length = $dimensions;
+class Furniture implements producttype
+{
+    public function __construct($sku, $name, $price, $type, $size, $weight, $dimensions)
+    {
+        $this -> sku = $sku;
+        $this -> price = $price;
+        $this -> name = $name;
+        $this -> type = $type;
+        $this -> dimensions = $dimensions;
 
-      
+
     }
-  public function addproduc(){
+  public function addproduc()
+  {
 
       $data= array('sku'=>$this->sku,'name'=>$this->name,'price'=>$this->price , 'type'=>$this->type , 'dimensions'=>$this->dimensions);
       $addproduct = new Contr();
@@ -57,13 +68,13 @@ class Furnitureh  implements producttype {
 }
 
 
- class Datadd{
+class Datadd
+{
+    public function attribute($sku, $name, $price, $type, $size, $weight, $dimensions)
+    {
 
- function attribute( $sku, $name, $price ,$type, $size, $weight, $dimensions) {
-
-    $class = $type;
-    $d =  new $class( $sku, $name, $price ,$type, $size, $weight, $dimensions);
-    $d->addproduc();
+        $class = $type;
+        $d =  new $class($sku, $name, $price, $type, $size, $weight, $dimensions);
+        $d->addproduc();
+    }
 }
- }
- 
