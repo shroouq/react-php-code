22b762420555478c3b9622b0891997d7cde2a5dc
shroouq <37282287+shroouq@users.noreply.github.com>
2023-04-28 19:17:25 -0700

    Update db.php

--- php/classes/db.php
+++ php/classes/db.php
@@ -1,19 +1,20 @@
 <?php
+
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
 
-class Db{
-
+class Db
+{
     private $host = "localhost";
     private $user = "root";
     private $password = "";
     private $dbName= "scandiweb_test";
 
-    protected function connect(){
+    protected function connect()
+    {
         $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
-        $pdo = new PDO ($dsn, $this->user, $this->password);
+        $pdo = new PDO($dsn, $this->user, $this->password);
         return $pdo;
     }
 }
-?>
