759313545612c3649a32c1d61dbba755fe0c2b77
shroouq <37282287+shroouq@users.noreply.github.com>
2023-04-28 19:27:11 -0700

    Update DvdItem.js

--- src/component/DvdItem.js
+++ src/component/DvdItem.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-export default function DvdItem(props){
-  return(
-    
+export default function DvdItem(props) {
+  return (
+
     <div className="product">
-    {props.sku}
-    <br/>
-    {props.name}
-    <br/>
-    {props.price } $
-    <br/>
-    Size: {props.size} MB
+      {props.sku}
+      <br />
+      {props.name}
+      <br />
+      {props.price} $
+      <br />
+      Size: {props.size} MB
 
     </div>
   )
