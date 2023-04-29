dff88b4adf901e24633e62190617bcd638a7a199
shroouq <37282287+shroouq@users.noreply.github.com>
2023-04-28 19:22:53 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Headr from './component/Headr';
-import Footr  from './component/Footr';
+import Footr from './component/Footr';
 
-function App(){
-  return(
-  <div className='App'>
-    
-    <Headr />
-    <Footr/>
-    
-  </div>
+function App() {
+  return (
+    <div className='App'>
+
+      <Headr />
+      <Footr />
+
+    </div>
   )
 }
 export default App;
