09a0a16413e20ece3c9bd53c616f25411ee8c3d1
shroouq <37282287+shroouq@users.noreply.github.com>
2023-04-28 19:28:21 -0700

    Update Furniture.js

--- src/component/Furniture.js
+++ src/component/Furniture.js
@@ -3,47 +3,50 @@ import React from "react";
 export default function Furniture(props) {
   return (
     <div >
-        <div className="group">
+      <div className="group">
         <label >Height (CM)</label>
         <span>{props.runHandleInvalid2}</span>
-          <input
-            id="height"
-            name="height"
-            className="data"
-            onChange={props.runHandleChange}
-            onKeyPress={props.handlenumeric} 
-            onPaste={props.handlenumeric}
-          />
-        </div>
-        
-      <br />
-        <div className="group">
-          <label >Width (CM)</label>
-          <input
-            id="width"
-            name="width"
-            className="data"
-            onChange={props.runHandleChange}
-            onKeyPress={props.handlenumeric} 
-            onPaste={props.handlenumeric}
-          />
-        <span>{props.runHandleInvalid3}</span>
-        </div>
-        
+        <input
+          id="height"
+          name="height"
+          className="data"
+          onChange={props.runHandleChange}
+          onInvalid={props.runHandleInvalid2}
+          onKeyPress={props.handlenumeric}
+          onPaste={props.handlenumeric}
+        />
+      </div>
+
       <br />
       <div className="group">
-          <label >Length (CM)</label>
-          <input
-            id="length"
-            name="length"
-            className="data"
-            onChange={props.runHandleChange}
-            onKeyPress={props.handlenumeric} 
-            onPaste={props.handlenumeric}
-          />
+        <label >Width (CM)</label>
+        <input
+          id="width"
+          name="width"
+          className="data"
+          onChange={props.runHandleChange}
+          onInvalid={props.runHandleInvalid3}
+          onKeyPress={props.handlenumeric}
+          onPaste={props.handlenumeric}
+        />
+        <span>{props.runHandleInvalid3}</span>
+      </div>
+
+      <br />
+      <div className="group">
+        <label >Length (CM)</label>
+        <input
+          id="length"
+          name="length"
+          className="data"
+          onChange={props.runHandleChange}
+          onInvalid={props.runHandleInvalid1}
+          onKeyPress={props.handlenumeric}
+          onPaste={props.handlenumeric}
+        />
         <span>{props.runHandleInvalid1}</span>
-        </div>
-        
+      </div>
+
       <div className="text">
         Please, provide dimensions in CM
       </div>
