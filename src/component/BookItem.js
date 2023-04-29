549ed81004139721eb91f21ea9eedde95542c3c1
shroouq <37282287+shroouq@users.noreply.github.com>
2023-04-28 19:26:17 -0700

    Update BookItem.js

--- src/component/BookItem.js
+++ src/component/BookItem.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-export default function DvdItem(props){
-  return(
-    
+export default function BookItem(props) {
+  return (
+
     <div className="product">
-    {props.sku}
-    <br/>
-    {props.name}
-    <br/>
-    {props.price } $
-    <br/>
-    Size: {props.size} MB
+      {props.sku}
+      <br />
+      {props.name}
+      <br />
+      {props.price} $
+      <br />
+      Weight: {props.weight} KG
 
     </div>
   )
