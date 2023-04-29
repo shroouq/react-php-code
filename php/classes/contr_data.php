a7c6b3bbf611b8398c38b1d49c8852230ff8e8b9
shroouq <37282287+shroouq@users.noreply.github.com>
2023-04-28 19:16:36 -0700

    Update contr_data.php

--- php/classes/contr_data.php
+++ php/classes/contr_data.php
@@ -2,29 +2,30 @@
 
 include_once("db_connect.php");
 
-class Contr extends Connect {
-
+class Contr extends Connect
+{
     //add product to database if the sku is not exist
-    public function addpro($sku,  $data){
-        if($this->check($sku) == false){
+    public function addpro($sku, $data)
+    {
+        if($this->check($sku) == false) {
             print_r("false");
             exit();
-        }
-       else{
+        } else {
             $this->setUser($data);
+            print_r("true");
             exit();
-       }
+        }
     }
+
     //get result from database if sku is exist or not
-    private function check($sku){
+    private function check($sku)
+    {
         $result= $this->checkSku($sku);
-        if($result > 0 ){
+        if($result > 0) {
             return false;
-        }
-        else{
+        } else {
 
-            return true;}
+            return true;
         }
     }
-
-            
+}
