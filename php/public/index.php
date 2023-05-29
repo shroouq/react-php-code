e3defd2ab946e66b867808856c4fdf553e1f8a47
shroouq <37282287+shroouq@users.noreply.github.com>
2023-05-28 19:16:27 -0700

    Create index.php

---
+++ php/public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require("../vendor/autoload.php");
+$router = new Router();
+
+$router->post('/php/classes/Contact', function(){
+    $contact = new Contact();
+    $contact->create();});
+
+$router->post('/php/controller/ControllerDelete', function(){
+    $contact = new ControllerDelete();
+    $contact->delete();});
+    
+$router->get('/php/classes/Contact', function(){
+    $contact = new Contact();
+    $contact->index();});
+
+
+
+$router->run();
+
